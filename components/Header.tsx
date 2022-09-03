9dae1457fece127b90357b68fc61157c9548a981
babar <systemrockstar96@gmail.com>
2022-09-03 13:20:25 +0530

    added scroll effect to header & added posts content

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
 function Header() {
+  const [top, setTop] = useState<Boolean>(true)
+
+  const handleScroll = () => {
+    const position = window.scrollY
+    if (position > 400) {
+      setTop(false)
+    } else if (position < 400) {
+      setTop(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header className='bg-yellow-400 border-b border-black sticky top-0'>
+    <header
+      className={`${
+        top ? 'bg-yellow-400' : 'bg-white'
+      } border-b border-black sticky top-0 transition duration-300 ease-linear`}>
       <nav className='flex justify-between p-5 max-w-7xl mx-auto'>
         <div className='flex items-center space-x-5'>
           <Link href='/'>
@@ -26,7 +48,11 @@ function Header() {
           <a href='#' className='hidden sm:flex'>
             Sign In
           </a>
-          <a href='#' className='text-white bg-black px-5 py-2 rounded-full'>
+          <a
+            href='#'
+            className={`${
+              top ? 'bg-black' : 'bg-green-700'
+            } text-white px-5 py-2 rounded-full transition duration-300 ease-linear`}>
             Get Started
           </a>
         </div>
